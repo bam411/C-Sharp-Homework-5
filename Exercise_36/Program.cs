4942098dc78f6268fdab4212b31636fb7072cee2
Petrushov Denis <bam411@yandex.ru>
2022-11-27 19:38:48 +0300

    exercise 36

---
+++ Exercise_36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Write("Введите размер массива: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+int RandomNumbers(int numbers, int min, int max)
+{
+    int[] randomNums = new int[numbers];
+    int sum = 0;
+   
+    for (int i = 0; i < randomNums.Length; i++)
+    {
+        randomNums[i] = new Random().Next(min, max);
+
+        Console.Write(randomNums[i] + " ");
+
+        if (i % 2 != 0)
+        {
+            sum += randomNums[i];
+        }
+    }
+    return sum;
+}
+
+int randomNums = RandomNumbers(numbers, -10, 10);
+
+Console.WriteLine($"\n{randomNums}");
